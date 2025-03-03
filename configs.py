17f82aed9f6a6d25a6c88b8de42e3e4f7a71148e
irfan-faisal <collagefaisal2@gmail.com>
2025-03-03 17:47:23 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -32,7 +32,7 @@ Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edi
 │
 ╰──────[ 😎 ]───────────⍟
 """
-  ABOUT_DEV_TEXT = f""" hi
+  ABOUT_DEV_TEXT = f""" Ho
 """
   HOME_TEXT = """
 Hello, [{}](tg://user?id={})\n\n **Hᴇʟʟᴏ Stranger🦋 ɪ ᴀᴍ ᴀ ᴘᴏᴡᴇʀꜰᴜʟ Fɪʟᴇ ᴛᴏ ʟɪɴᴋ + ꜰɪʟᴇ sᴛᴏʀᴇ ʙᴏᴛ **⚡️
